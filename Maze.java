7763010e13d320183dc158ab3261c181ab414904
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-26 17:50:26 -0500

    finished converter

--- Maze.java
+++ Maze.java
@@ -1,3 +1,20 @@
 public class Maze{
+  public String[][] ary;
+  public void convert(String text){
+    File fil = new File(text);
+    Scanner scan = new Scanner(fil);
+    String output = "";
+    while(scan.hasNext()){
+      output += scan.nextLine();}
+    scan = new Scanner(fil);
+    ary = new String[scan.nextLine().length()][output.length() / scan.length()];
+    scan = new Scanner(fil);
+    for(int i = 0; i < ary.length; i++){
+      for(int j = 0; j < ary[0].length; j++){
+        ary[i][j] = scan.next();}}}
+
+  public Maze(String text){
+    convert(text);}
+  
 
 }
