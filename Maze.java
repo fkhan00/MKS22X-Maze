18c47898446c51055afefc0ebe68d5b691043ed4
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-27 11:16:44 -0500

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Maze{
   public String[][] ary;
-  public int strX, strY;
+  public int strX, strY, counter;
   public static final int moves[] = {1, -1};
 
   public void convert(String text) throws FileNotFoundException{
@@ -24,12 +24,13 @@ public class Maze{
       }}}
 
   public Maze(String text) throws FileNotFoundException{
+    counter = 0;
     convert(text);
     for(int i = 0; i < ary.length; i++){
       for(int j = 0; j < ary[0].length; j++){
         if(ary[i][j].equals("S")){
           strY = i;
-          strX = j;}}}}
+          strX = j + 1;}}}}
 
   public boolean valid(int[][] covered, int row, int col){
     return row >= 0 && row < ary.length && col >=  0 && col < ary[0].length && covered[row][col] == 0 &&  (!ary[row][col].equals("#"));
@@ -41,18 +42,22 @@ public class Maze{
         output += ary[i][j];}}
     return output;
   }
-  public boolean solve(){
+  public int solve(){
     System.out.println(strY + " " + strX);
-    return solveH(strY, strX, new int[ary.length][ary[0].length], 0, 0);}
+    solveH(strY, strX, new int[ary.length][ary[0].length], 0, 0);
+    return counter;}
+
 
   public boolean solveH(int row, int col, int[][] covered, int dirR, int dirC){
       if((ary[row][col].equals("E")))
       {return true;}
     if(! valid(covered, row, col)){
+      counter --;
       return false;}
+    counter ++;
     covered[row][col] ++;
     ary[row][col] = "-";
-    return solveH(row + 1, col, covered, 1, 0) || solveH(row, col + 1, covered, 0, 1)
+     return solveH(row + 1, col, covered, 1, 0) || solveH(row, col + 1, covered, 0, 1)
     || solveH(row - 1, col, covered, -1, 0) || solveH(row, col - 1, covered, 0, -1);
 
       }
