7c5dd7f1458a225ef331007aaaac0265128fd822
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-28 20:19:08 -0500

    everything's good except for small bug in converter

--- Maze.java
+++ Maze.java
@@ -3,37 +3,38 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Maze{
   public String[][] ary;
-  public int strX, strY, counter;
-  public static final int moves[] = {1, -1};
+  public int strX, strY;
+  public int counter;
+  public static final int movesX[] = {1, -1, 0, 0};
+  public static final int movesY[] = {0, 0, 1, -1};
 
   public void convert(String text) throws FileNotFoundException{
+    counter = 0;
     File fil = new File(text);
     Scanner scan = new Scanner(fil);
     String output = "";
     while(scan.hasNextLine()){
-      output += scan.nextLine();
-      output += "\n";
-    }
+      output += scan.nextLine();}
     scan = new Scanner(fil);
-    ary = new String[output.length() / (scan.nextLine().length() + 1)][scan.nextLine().length()];
+    ary = new String[output.length() / scan.nextLine().length()][scan.nextLine().length()];
+    scan = new Scanner(fil);
     int counter = 0;
     for(int i = 0; i < ary.length; i++){
       for(int j = 0; j < ary[0].length; j++){
-        ary[i][j] = output.substring(counter, counter + 1);
+        ary[i][j] = scan.next();
         counter ++;
       }}}
 
   public Maze(String text) throws FileNotFoundException{
-    counter = 0;
     convert(text);
     for(int i = 0; i < ary.length; i++){
       for(int j = 0; j < ary[0].length; j++){
         if(ary[i][j].equals("S")){
           strY = i;
-          strX = j + 1;}}}}
+          strX = j;}}}}
 
-  public boolean valid(int[][] covered, int row, int col){
-    return row >= 0 && row < ary.length && col >=  0 && col < ary[0].length && covered[row][col] == 0 &&  (!ary[row][col].equals("#"));
+  public boolean valid( int row, int col){
+    return row >= 0 && row < ary.length && col >=  0 && col < ary[0].length && ! ary[row][col].equals("@") &&  (!ary[row][col].equals("#"));
   }
   public String toString(){
     String output = "";
@@ -42,25 +43,26 @@ public class Maze{
         output += ary[i][j];}}
     return output;
   }
-  public int solve(){
-    System.out.println(strY + " " + strX);
-    solveH(strY, strX, new int[ary.length][ary[0].length], 0, 0);
-    return counter;}
+  public boolean solve(){
+    return solveH(strY, strX, 0);}
 
-
-  public boolean solveH(int row, int col, int[][] covered, int dirR, int dirC){
-      if((ary[row][col].equals("E")))
-      {return true;}
-    if(! valid(covered, row, col)){
-      counter --;
+  public boolean solveH(int row, int col, int steps){
+    if(ary[row][col].equals("E")){
+      return true;}
+    System.out.println(toString());
+    if(valid(row, col)){
+      ary[row][col] = "@";
+      if(solveH(row + 1, col, steps + 1)){
+        return true;}
+      if(solveH(row - 1, col, steps + 1)){
+          return true;}
+      if(solveH(row , col + 1, steps + 1)){
+        return true;}
+      if(solveH(row , col - 1, steps + 1)){
+        return true;}
+      ary[row][col] = ".";
       return false;}
-    counter ++;
-    covered[row][col] ++;
-    ary[row][col] = "-";
-     return solveH(row + 1, col, covered, 1, 0) || solveH(row, col + 1, covered, 0, 1)
-    || solveH(row - 1, col, covered, -1, 0) || solveH(row, col - 1, covered, 0, -1);
-
-      }
+    return false;}
 
 
 }
